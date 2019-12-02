c98c7994659d6d5dc7fe51e4e781977f1adcab4d
Martin Otzen <mao@danfoss.com>
2019-12-02 13:32:09 +0100

    minor update

--- BuildDepBuilder/FetchCr.py
+++ BuildDepBuilder/FetchCr.py
@@ -2,6 +2,12 @@ import os
 
 from products import products
 
+def NeedUpdate( source, target ):
+	if os.path.exists( target ) and os.path.getctime( source ) < os.path.getctime( target ) :
+		return False
+	else :
+		return True
+
 if __name__ == '__main__':
 	crDIr = 'crs'
 	if not os.path.isdir( crDIr ) :
@@ -15,13 +21,26 @@ if __name__ == '__main__':
 	os.system( 'cleartool mount \\export' )
 	os.system( 'cleartool mount \\pfpd' )
 	os.system( 'cleartool mount \\pnio_dk' )
+	os.system( 'cleartool mount \\fpga' )
 	os.system( 'cleartool mount \\bacnetStack' )
 	
 	for p in products :
-		doPathName = os.path.join('M:\mah__main', products[ p ] )
-		crPathName = os.path.join( crDIr, p + '.cr' ) 
-		cmd = 'cleartool catcr -recurse -ci -type fl -long ' + doPathName + ' > '  + crPathName
-		print( cmd )
-		os.system( cmd )
-	
+		doPathName = os.path.join('M:\mao__cr_extract', products[ p ] )
+		crPathName = os.path.join( crDIr, p + '.cr' )
+		if os.path.exists( doPathName ) :
+			if NeedUpdate( doPathName, crPathName ) : 
+				cmd = 'cleartool catcr -recurse -ci -type fl -long ' + doPathName + ' > '  + crPathName
+				#print( cmd )
+				exitCode = os.system( cmd )
+				if exitCode > 0 :
+					os.remove( crPathName )
+					print( 'ERROR:', crPathName )
+				else :
+					print( 'created', crPathName )
+			else :
+				print( '\tskipping {}',format( crPathName ) )
+
+		else :
+			print( "WARNING Can't find:", doPathName )
+			
 		
---
+++ ConfigRec2Dot/crs/@manualWoodoo.cr
@@ -0,0 +1,56 @@
+----------------------------
+MVFS objects:
+----------------------------
+view private object    \export\p400\TESTMON_AOC\1_45\TESTMON_AOC_1_45.s1              [new derived object]
+view private object    \export\p400\TESTMON_AOC\TESTMON_AOC_1_53\TESTMON_AOC_1_53.s1  [new derived object]
+derived object         \p400\product\testmonitor\build\bootstrap_redboot.s1           [referenced derived object]
+----------------------------
+Build Script:
+----------------------------
+    Maual woodoo
+----------------------------
+
+----------------------------
+MVFS objects:
+----------------------------
+view private object    \export\p400\FAN_POWERCARD_TM\FAN_POWERCARD_TM_1_22\FAN_POWERCARD_TM_1_22.s  [new derived object]
+derived object         \p400\product\poweroption_testmon\FanPowerCard_TM\build\FanPowerCard_TM.s    [referenced derived object]
+----------------------------
+Build Script:
+----------------------------
+    Maual woodoo
+---------------------------- 
+ 
+----------------------------
+MVFS objects:
+----------------------------
+view private object    \export\p400\TESTMON_CC_MKII\TESTMON_CC_MKII_2_01\TESTMON_CC_MKII_2_01.s1   [new derived object]
+derived object         \p400\product\testmonitor\controlcard\cc_mkII\build\testmonitor_cc_mkII.s1  [referenced derived object]
+----------------------------
+Build Script:
+----------------------------
+    Maual woodoo
+---------------------------- 
+  
+----------------------------
+MVFS objects:
+----------------------------
+view private object    \export\p400\option_tm_v1_088.s1                                    [new derived object]
+derived object         \p400\product\option_testmon\other_options\build\option_testmon.s1  [referenced derived object]
+----------------------------
+Build Script:
+----------------------------
+    Maual woodoo
+---------------------------- 
+
+----------------------------
+MVFS objects:
+----------------------------
+view private object    \export\p400\TESTMON_CC_MKII\TESTMON_CC_MKII_2_05_NEW_FLASH_RC1_00\TESTMON_CC_MKII_2_05.s1   [new derived object]
+derived object         \p400\product\testmonitor\controlcard\cc_mkII\build\testmonitor_cc_mkII.s1                   [referenced derived object]
+----------------------------
+Build Script:
+----------------------------
+    Maual woodoo
+---------------------------- 
+
